0e82b7b53a80020fc5ee6df2e12c0428dc103706
manupumarr <manupisopumarr@gmail.com>
2026-02-03 22:50:11 -0400

    SitePlan

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,6 @@
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+currentYear.textContent = today.getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
